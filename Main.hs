297d3c7311274f7471d57072a72848976a9657fe
Paul Rijneveld <paulrijneveld@gmail.com>
2022-12-15 20:20:37 +0000

    Format better

--- Main.hs
+++ Main.hs
@@ -1,7 +1,5 @@
-{-# LANGUAGE ScopedTypeVariables #-}
-
-import System.IO
 import Debug.Trace
+import System.IO
 
 data Instruction = Addx Int | Noop deriving (Show)
 
@@ -10,9 +8,9 @@ main = do
   let instructions = map toInstr $ lines contents
       withCycles = map (\i -> (i, cycleTime i, xChange i)) instructions
       withTotals = scanl (\(i1, c1, x1) (i2, c2, x2) -> (i2, c1 + c2, x1 + x2)) (Noop, 0, 1) withCycles
-      withSignal = map (\(i, c, x) -> (i, c, x, closestTo20Mod40 c * x)) withTotals
-      on20mod40 = keepWhen 
-          (\(i, c, x, s) -> c `mod` 40 < 20) 
+      withSignal = map (\(i, c, x) -> (i, c, x, to20Mod40 c * x)) withTotals
+      on20mod40 = keepWhen
+          (\(i, c, x, s) -> c `mod` 40 < 20)
           (\(i, c, x, s) -> c `mod` 40 == 20 || c `mod` 40 == 21)
           withSignal
       signals = map (\(i, c, x, s) -> s) on20mod40
@@ -34,9 +32,9 @@ xChange (Addx change) = change
 keepWhen :: (a -> Bool) -> (a -> Bool) -> [a] -> [a]
 keepWhen p q [] = []
 keepWhen p q [x] = []
-keepWhen p q (x1:x2:xs) = if p x1 && q x2 then x1 : keepWhen p q (x2:xs) else keepWhen p q (x2:xs)
+keepWhen p q (x1 : x2 : xs)
+  | p x1 && q x2 = x1 : keepWhen p q (x2 : xs)
+  | otherwise = keepWhen p q (x2 : xs)
 
-closestTo20Mod40 :: Int -> Int
-closestTo20Mod40 n = let res = n `mod` 40
-                         diff = 20 - res
-                         in n + diff
+to20Mod40 :: Int -> Int
+to20Mod40 n = n - n `mod` 40 + 20
