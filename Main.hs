16c962f293df212c2231dfe5d8495c4da70665c1
Paul Rijneveld <paulrijneveld@gmail.com>
2022-12-15 16:37:22 +0000

    Add cycle time

--- Main.hs
+++ Main.hs
@@ -1,12 +1,17 @@
 import System.IO
 
-data Instruction = Addx Int | Noop deriving Show
+data Instruction = Addx Int | Noop deriving (Show)
 
 main = do
   contents <- readFile "Example.txt"
   let instructions = map toInstr $ lines $ contents
+      withCycles = map (\x -> (x, cycleTime x)) instructions
   print instructions
 
 toInstr :: String -> Instruction
 toInstr "noop" = Noop
 toInstr add = Addx $ read $ head $ tail $ words add
+
+cycleTime :: Instruction -> Int
+cycleTime Noop = 1
+cycleTime (Addx _) = 2
