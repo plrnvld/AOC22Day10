d2609ab3d93c8cc7fe9b382cc0cf0cb1da7e383d
Paul Rijneveld <paulrijneveld@gmail.com>
2022-12-15 18:37:58 +0000

    Calculcate signal

--- Main.hs
+++ Main.hs
@@ -5,8 +5,10 @@ data Instruction = Addx Int | Noop deriving (Show)
 main = do
   contents <- readFile "Example.txt"
   let instructions = map toInstr $ lines $ contents
-      withCycles = map (\x -> (x, cycleTime x)) instructions
-  print instructions
+      withCycles = map (\i -> (i, cycleTime i, xChange i)) instructions
+      withTotals = scanl1 (\(i1, c1, x1) (i2, c2, x2) -> (i2, c1 + c2, x1 + x2)) withCycles
+      withSignal = map (\(i, c, x) -> (i, c, x, c * x)) withTotals
+  print withSignal
 
 toInstr :: String -> Instruction
 toInstr "noop" = Noop
@@ -15,3 +17,7 @@ toInstr add = Addx $ read $ head $ tail $ words add
 cycleTime :: Instruction -> Int
 cycleTime Noop = 1
 cycleTime (Addx _) = 2
+
+xChange :: Instruction -> Int
+xChange Noop = 0
+xChange (Addx change) = change
